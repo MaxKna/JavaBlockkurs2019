133f6eff6f7e861f9da33bc19b642a93bc3a5dee
mv4982s <mariusvogel1@gmail.com>
2019-09-18 13:34:32 +0200

    fibonacci iterative

--- src/Probeklausur/a2.java
+++ src/Probeklausur/a2.java
@@ -2,11 +2,11 @@ package Probeklausur;
 
 public class a2 {
     //Soll das größte Element zurückgeben welches in beiden Array vorhanden ist
-    public static int getBiggestCommonElement(int[][] a, int[][] b){
+    public static int getBiggestCommonElement(int[][] a, int[][] b) {
         int ret = Integer.MIN_VALUE;
-        for (int[] innerA : a){
-            for(int innerIntA : innerA){
-                if(innerIntA > ret && scanArray(innerIntA, b)){
+        for (int[] innerA : a) {
+            for (int innerIntA : innerA) {
+                if (innerIntA > ret && scanArray(innerIntA, b)) {
                     ret = innerIntA;
                 }
             }
@@ -15,13 +15,13 @@ public class a2 {
     }
 
 
-    private static boolean scanArray(int i, int[][] arr){
-        for (int[] innerArr : arr){
-           for (int innerI : innerArr){
-               if(i == innerI){
-                   return true;
-               }
-           }
+    private static boolean scanArray(int i, int[][] arr) {
+        for (int[] innerArr : arr) {
+            for (int innerI : innerArr) {
+                if (i == innerI) {
+                    return true;
+                }
+            }
         }
         return false;
     }
