61be7d711f96599da576af38c8bacfe07d5257b4
mv4982s <mariusvogel1@gmail.com>
2019-09-18 11:50:37 +0200

    Bugfix

--- src/Probeklausur/a4.java
+++ src/Probeklausur/a4.java
@@ -5,10 +5,36 @@ public class a4 {
         double erg = 0.;
         switch (c1) {
             case '*':
-                erg += d1 * d2;
+                switch (c2) {
+                    case '+':
+                        erg += (d1 * d2) + d3;
+                        break;
+                    case '-':
+                        erg += d1 - d2 - d3;
+                    case '*':
+                        erg += (d1 * d2) * d3;
+                        break;
+                    case '/':
+                        erg += (d1 * d2) / d3;
+                    default:
+                        throw new ArithmeticException("Fehler");
+                }
                 break;
             case '/':
-                erg += d1 / d2;
+                switch (c2) {
+                    case '+':
+                        erg += d1 / d2 + d3;
+                        break;
+                    case '-':
+                        erg += d1 / d2 - d3;
+                    case '*':
+                        erg += (d1 / d2) * d3;
+                        break;
+                    case '/':
+                        erg += (d1 / d2) / d3;
+                    default:
+                        throw new ArithmeticException("Fehler");
+                }
                 break;
             case '+':
                 switch (c2) {
@@ -50,6 +76,6 @@ public class a4 {
     }
 
     public static void main(String[] args) {
-        System.out.println(werteAus(-1.,'+', 2., '*', 3.));
+        System.out.println(werteAus(-1., '+', 2., '*', 3.));
     }
 }
