5a2c75202e561fa73c35a88dfaea432a5d4ead2d
mv4982s <mariusvogel1@gmail.com>
2019-09-18 12:16:29 +0200

    ups was vergessen

--- src/Probeklausur/Text.java
+++ src/Probeklausur/Text.java
@@ -8,12 +8,12 @@ public class Text {
     private String text;
 
     //erstellt ein Textobjekt aus übergebenen String
-    public Text(String s){
+    public Text(String s) {
         this.setText(s);
     }
 
     //erstellt ein Textobjekt aus übergebenen Text
-    public Text(Text t){
+    public Text(Text t) {
         this.setText(t.getText());
     }
 
@@ -26,21 +26,34 @@ public class Text {
     }
 
     //gib den char an Stelle i zurück bei neg. Zahlen von hinten
-    public char getCharAt(int i){
-        if(i < 0){
-            return this.text.charAt(this.text.length()+i);
+    public char getCharAt(int i) {
+        if (i < 0) {
+            return this.text.charAt(this.text.length() + i);
         }
         return this.text.charAt(i);
     }
 
     //Zählt wie oft c in text vorkommt
-    public int getCount(char c){
+    public int getCount(char c) {
         int counter = 0;
-        for (int i = 0; i<this.text.length(); i++){
-            if(this.text.charAt(i) == c){
+        for (int i = 0; i < this.text.length(); i++) {
+            if (this.text.charAt(i) == c) {
                 counter++;
             }
         }
         return counter;
+    }
+
+    //gibt das n-te vorkommen des zeichen c zurück
+    public int getPos(char c, int i) {
+        for (int j = 0; j < this.text.length(); j++) {
+            if (this.text.charAt(j) == c) {
+                i--;
+                if (i == 0) {
+                    return i;
+                }
+            }
+        }
+        throw new ArrayIndexOutOfBoundsException("kein " + i + "-es " + c + " gefunden");
     }
 }
